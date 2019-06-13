08597e1687f2ffe5a31cd2b43ca69361bc9b495e
RomekRJM <romek.rjm@gmail.com>
2019-06-13 21:31:09 +0200

    Sight of view for Foe. Player reacts so on input.

---
+++ Area2D.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+export var speed = 400
+var screen_size
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
+		velocity.x = -1
+	if Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
+		velocity.x = 1
+	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
+		velocity.y = -1
+	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
+		velocity.y = 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
