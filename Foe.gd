7e76578ffd06142dafbc7dcf326deeb0b2aa5aab
RomekRJM <romek.rjm@gmail.com>
2019-06-15 11:19:38 +0200

    Remove unused resources.

--- Foe.gd
+++ Foe.gd
@@ -9,5 +9,5 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	follow.
+#func _process(delta):
+#	pass
