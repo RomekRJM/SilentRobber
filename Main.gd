ba5415b69627f1f0a4754741a129948c84924cc7
RomekRJM <romek.rjm@gmail.com>
2019-06-16 19:11:42 +0200

    Auto register all enemy collisions on root node.

--- Main.gd
+++ Main.gd
@@ -6,7 +6,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Camera").get_node("VisionBody").connect("player_spoted", self, "_player_spoted")
+	for enemy in get_tree().get_nodes_in_group("Enemies"):
+		enemy.find_node("VisionBody").connect("player_spoted", self, "_player_spoted")
 
 func _player_spoted():
 	get_tree().reload_current_scene()
