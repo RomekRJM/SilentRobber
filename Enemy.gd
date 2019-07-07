9f23dd63709a88a78750c9c6a6de2273a8c86ddc
RomekRJM <romek.rjm@gmail.com>
2019-07-07 10:24:24 +0200

    Experiments.

--- Enemy.gd
+++ Enemy.gd
@@ -9,6 +9,7 @@ const deg1 = PI / 180.0
 const step = deg1
 const rays = 90
 const mid_ray = rays / 2
+var perpendicular
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +23,9 @@ func _physics_process(delta):
 
 	if result:
 		hit_pos = result.position
+		var normalized = hit_pos.normalized()
+		perpendicular = Vector2(-normalized.y*200, normalized.x*200)
+		print(hit_pos.length())
 		update()
 		
 		if result.collider.name == "Player":
@@ -31,6 +35,7 @@ func _draw():
 	if hit_pos:
 		var pos = (hit_pos - position).rotated(-rotation)
 		draw_line(Vector2(0.0, 0.0), pos, laser_color)
+		draw_line(pos, perpendicular, laser_color)
 		draw_circle(pos, 5, laser_color)
 
 		for i in range(rays):
