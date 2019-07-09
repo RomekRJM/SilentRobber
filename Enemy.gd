0da60b55bc7be884b0c55799d6d8df134de9ad1d
RomekRJM <romek.rjm@gmail.com>
2019-07-09 12:13:13 +0200

    Shorten rays, when it hits an obstacle.

--- Enemy.gd
+++ Enemy.gd
@@ -3,15 +3,19 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 var beta
-var p
 var c
 var laser_color = Color(1.0, 0.5, 0.6)
 var hit_pos = []
-const r = 50
-const tau = 2 * PI 
-const alpha = tau / 6
+const r = 200
+const alpha = TAU / 6
 const rays = 60
 const sight_step = alpha * 2 / rays
+
+var rot = 0.0
+var direction = 1
+var speed = 2.2
+const MAX_ANGLE = PI
+const MIN_ANGLE = -MAX_ANGLE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,34 +24,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(position, get_tree().get_current_scene().get_node("Player").position, 
-		[self], collision_mask)
-
-	if result:
-		p = result.position
-		c = position
-		beta = (c-p).angle()
+	rot += direction * delta * speed
+	rot = clamp(rot, MIN_ANGLE, MAX_ANGLE)
+	
+	if rot >= MAX_ANGLE:
+		direction = -1
+	elif rot <= MIN_ANGLE:
+		direction = 1
 		
-		for i in range(rays):
-			var current_angle = beta - alpha + i * sight_step
-			var ray_destination = Vector2(cos(current_angle) * r + c.x, sin(current_angle) * r + c.y)
-			var final_ray_destination = space_state.intersect_ray(position, ray_destination, [self], collision_mask)
-			
-			if final_ray_destination:
-				hit_pos[i] = final_ray_destination.position
-			else:
-				hit_pos[i] = ray_destination
-			
-		update()
+	for i in range(rays):
+		var current_angle = rot - alpha + i * sight_step
+		var ray_destination = Vector2(cos(current_angle) * r + position.x, sin(current_angle) * r + position.y)
+		var final_ray_destination = space_state.intersect_ray(position, ray_destination, [self], collision_mask)
 		
-		if result.collider.name == "Player":
-			pass
+		if final_ray_destination:
+			hit_pos[i] = final_ray_destination.position
+		else:
+			hit_pos[i] = ray_destination
+		
+	update()
 		
 func _draw():
-	if p:
-		var pos = (p - position).rotated(-rotation)
-		draw_line(Vector2(0.0, 0.0), pos, laser_color)
-		draw_circle(pos, 5, laser_color)
-
-		for h in hit_pos:
-			draw_line(Vector2(0.0, 0.0), (p-h).rotated(-rotation), laser_color)
+	for h in hit_pos:
+		if h:
+			draw_line(Vector2(0.0, 0.0), (h-position).rotated(-rotation), laser_color)
