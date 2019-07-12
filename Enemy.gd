9aa8fa912a92b144cad3dcc5b37927569e4ecb85
RomekRJM <romek.rjm@gmail.com>
2019-07-12 12:41:08 +0200

    Rotate vision with the enemy on PathFollow2D.

--- Enemy.gd
+++ Enemy.gd
@@ -7,9 +7,13 @@ var c
 var laser_color = Color(1.0, 0.5, 0.6)
 var hit_pos = []
 const r = 200
-const alpha = TAU / 6
-const rays = 60
-const sight_step = alpha * 2 / rays
+
+# This is half of vision angle
+const HALF_OF_VISION_ANGLE = TAU / 9
+
+# How many rays should be casted inside vision angle
+const RAYS_IN_VISION_ANGLE = 60
+const RAYS_ANGLE_INTERVAL = HALF_OF_VISION_ANGLE * 2 / RAYS_IN_VISION_ANGLE
 
 var rot = 0.0
 var direction = 1
@@ -17,30 +21,31 @@ var speed = 2.2
 var vision_polygon
 var poolVector2Array
 var current_hit_pos
-const MAX_ANGLE = PI/2
+const MAX_ANGLE = PI/4
 const MIN_ANGLE = -MAX_ANGLE
 
 signal player_spoted
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hit_pos.resize(rays+1)
+	hit_pos.resize(RAYS_IN_VISION_ANGLE+1)
 	vision_polygon = get_node("VisionBody/VisionPolygon")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var space_state = get_world_2d().direct_space_state
 	rot += direction * delta * speed
+	var global_rot = rot - global_rotation
 	
-	if rot >= MAX_ANGLE:
+	if global_rot >= MAX_ANGLE:
 		direction = -1
-	elif rot <= MIN_ANGLE:
+	elif global_rot <= MIN_ANGLE:
 		direction = 1
 	
 	hit_pos[0] = Vector2(0.0, 0.0)
 	
-	for i in range(rays):
-		var current_angle = rot - alpha + i * sight_step
+	for i in range(RAYS_IN_VISION_ANGLE):
+		var current_angle = rot - HALF_OF_VISION_ANGLE + i * RAYS_ANGLE_INTERVAL
 		var ray_destination = Vector2(cos(current_angle) * r + global_position.x, sin(current_angle) * r + global_position.y)
 		var final_ray_destination = space_state.intersect_ray(global_position, ray_destination, [self], collision_mask)
 		
