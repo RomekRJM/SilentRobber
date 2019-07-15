7f9eacb0fc250de128de39b60ed9b132f2528dcf
RomekRJM <romek.rjm@gmail.com>
2019-07-15 13:26:16 +0200

    Reenable level restart, when someone sees player.

--- Main.gd
+++ Main.gd
@@ -7,8 +7,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for enemy in get_tree().get_nodes_in_group("Enemies"):
-		# enemy.find_node("Enemy").connect("player_spoted", self, "_player_spoted")
-		enemy.find_node("Enemy")
+		enemy.connect("player_spoted", self, "_player_spoted")
+
 
 func _player_spoted():
 	get_tree().reload_current_scene()
