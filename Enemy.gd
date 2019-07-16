cbf048558af18a625c2fee142d1bd9503ae36ed9
RomekRJM <romek.rjm@gmail.com>
2019-07-16 11:29:45 +0200

    Enemies vars to be exposed for Godot editor.

--- Enemy.gd
+++ Enemy.gd
@@ -1,28 +1,26 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
+# This is half of vision angle
+export var vision_angle = 60
+export var r = 200
+export var rot = 0.0
+export var direction = 1
+export var speed = 2.2
+
+# How many rays should be casted inside vision angle
+const RAYS_IN_VISION_ANGLE = 60
+const MAX_ANGLE = PI/4
+const MIN_ANGLE = -MAX_ANGLE
+
 var beta
 var c
 var laser_color = Color(1.0, 0.5, 0.6)
 var hit_pos = []
-const r = 200
-
-# This is half of vision angle
-const HALF_OF_VISION_ANGLE = TAU / 9
-
-# How many rays should be casted inside vision angle
-const RAYS_IN_VISION_ANGLE = 60
-const RAYS_ANGLE_INTERVAL = HALF_OF_VISION_ANGLE * 2 / RAYS_IN_VISION_ANGLE
-
-var rot = 0.0
-var direction = 1
-var speed = 2.2
 var vision_polygon
 var poolVector2Array
 var current_hit_pos
-const MAX_ANGLE = PI/4
-const MIN_ANGLE = -MAX_ANGLE
+var half_of_vision_angle = deg2rad(vision_angle / 2.0)
+var rays_angle_interval = half_of_vision_angle * 2 / RAYS_IN_VISION_ANGLE
 
 signal player_spoted
 
@@ -45,7 +43,7 @@ func _physics_process(delta):
 	hit_pos[0] = Vector2(0.0, 0.0)
 	
 	for i in range(RAYS_IN_VISION_ANGLE):
-		var current_angle = rot - HALF_OF_VISION_ANGLE + i * RAYS_ANGLE_INTERVAL
+		var current_angle = rot - half_of_vision_angle + i * rays_angle_interval
 		var ray_destination = Vector2(cos(current_angle) * r + global_position.x, sin(current_angle) * r + global_position.y)
 		var final_ray_destination = space_state.intersect_ray(global_position, ray_destination, [self], collision_mask)
 		
